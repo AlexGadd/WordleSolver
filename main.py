60b20cc3c2251f59098a62f9cfc985546f567b21
Alex <gadda023@gmail.com>
2022-06-01 19:10:30 +1200

    Added filter for unwanted letters

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+#Importing libraries
 import urllib.request, urllib.error, urllib.parse
 url = "https://static.nytimes.com/newsgraphics/2022/01/25/wordle-solver/assets/solutions.txt"
 
+#Pulling word list
 response = urllib.request.urlopen(url)
 WordList = response.read().decode('UTF-8').splitlines()
 
-GoodLetters = input("Please input letters that appear in the word")
-# BadLetters = input("Please input letters that do not appear in the word")
+#Splits strings into characters
+def split(word):
+    return [char for char in word]
+
+#Keeps words with correct characters only
+def FilterWanted(Chars,lst):
+    for char in Chars:
+        lst = list(filter(lambda k: char in k, lst))
+    return lst
+
+#removes words with incorrect characters
+def FilterUnwanted(Chars,lst):
+    for char in Chars:
+        lst = list(filter(lambda k: char not in k, lst))
+    return lst
+
+#Taking inputs
+GoodLetters = split(input("Please input letters that appear in the word"))
+BadLetters = split(input("Please input letters that do not appear in the word"))
 # InPlaceChar = input("Please input letters in their respective positions, using a '_' for blank spaces")
 intWordList = []
 WordsLeft = []
 
-for word in WordList:
-    for char in GoodLetters:
-        if char in word:
-            intWordList.append(word)
+#Filtering with good letters
+WordsLeft = FilterWanted(GoodLetters, WordList)
+WordsLeft = FilterUnwanted(BadLetters,WordsLeft)
+
 
 print(f"Possible words left are {WordsLeft}")
 
 
 
-# list(filter(lambda k: 'ab' in k, lst))
+

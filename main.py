58a1bd40842bb4cf364d94893c94845ceb3302f3
Alex <gadda023@gmail.com>
2022-07-03 16:53:39 +1200

    Adding letter in specific place filter - currently not working, not sure I'm using the lambda function correctly

--- main.py
+++ main.py
@@ -22,16 +22,25 @@ def FilterUnwanted(Chars,lst):
         lst = list(filter(lambda k: char not in k, lst))
     return lst
 
+def LettersInPlace(Chars, lst):
+    pos = 0
+    for char in Chars:
+        if(char.isalpha()):
+            lst = list(filter(lambda k: k.index(char) == pos, lst))
+        pos += 1
+
 #Taking inputs
 GoodLetters = split(input("Please input letters that appear in the word"))
 BadLetters = split(input("Please input letters that do not appear in the word"))
-# InPlaceChar = input("Please input letters in their respective positions, using a '_' for blank spaces")
+InPlaceChar = split(input("Please input letters in their respective positions, using a '-' for blank spaces"))
 intWordList = []
 WordsLeft = []
 
 #Filtering with good letters
 WordsLeft = FilterWanted(GoodLetters, WordList)
+#Filtering with bad letters
 WordsLeft = FilterUnwanted(BadLetters,WordsLeft)
+WordsLeft = LettersInPlace(InPlaceChar, WordsLeft)
 
 
 print(f"Possible words left are {WordsLeft}")
